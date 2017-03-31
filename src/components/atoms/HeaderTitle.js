9a653c32653a338340afe031576ade37b08f6f09
alanzanattadev <alan.zanatta@outlook.com>
2017-03-31 23:11:17 +0200

    Increase font size of header title for mdpi screens

--- src/components/atoms/HeaderTitle.js
+++ src/components/atoms/HeaderTitle.js
@@ -8,7 +8,7 @@ import { PrimaryLightFont } from "../nucleons/texts";
 
 export default styled.h1`
   font-family: ${PrimaryLightFont};
-  font-size: 50px;
+  font-size: 60px;
   font-weight: 100;
   color: ${WHITE};
   margin: 80px 20px 80px 20px;
