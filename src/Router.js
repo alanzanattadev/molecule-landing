73282283659490865451dec53988ee73a4011641
Theomorais <theo.morais@epitech.eu>
2017-05-05 17:56:43 +0200

    Add markdown loading for documentation

--- src/Router.js
+++ src/Router.js
@@ -1,22 +1,17 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link
-} from 'react-router-dom'
-import Home from './Home';
-import Documentation from './components/doc/Documentation'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Home from "./Home";
+import Documentation from "./components/doc/Documentation";
 
 const AppRouter = () => (
   <Router>
     <div>
       <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/document" component={Documentation}/>
+        <Route exact path="/" component={Home} />
+        <Route path="/document" component={Documentation} />
       </Switch>
     </div>
   </Router>
-)
+);
 
-export default AppRouter
+export default AppRouter;
--- src/components/doc/Documentation.js
+++ src/components/doc/Documentation.js
@@ -1,37 +1,41 @@
 "use babel";
 // @flow
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link
-} from 'react-router-dom'
-import './Documentation.css'
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Markdown from "react-markdown";
+
+//import Markdown
+import element1 from "./../../../public/documentMarkdown/Element1.md";
+import element2 from "./../../../public/documentMarkdown/Element2.md";
+
+import "./Documentation.css";
 
 class Documentation extends Component {
-    render(){
-      return (
-        <div>
-          <div className="leftMenu">
-            <ul>
-              <li><Link to="/document">Element1</Link></li>
-              <li><Link to="/document/elem2">Element2</Link></li>
-            </ul>
-          </div>
-          <div className="docContent">
-            <Switch>
-            <Route exact path="/document" component={Elem1}/>
-            <Route exact path="/document/elem2" component={Elem2}/>
-            </Switch>
-          </div>
+  render() {
+    return (
+      <div>
+        <div className="leftMenu">
+          <ul>
+            <li><Link to="/document">Element1</Link></li>
+            <li><Link to="/document/elem2">Element2</Link></li>
+          </ul>
         </div>
-      )
-    }
-
-
-
+        <div className="docContent">
+          <Switch>
+            <Route exact path="/document" component={Elem1} />
+            <Route exact path="/document/elem2" component={Elem2} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
-const Elem1 = () => <h3>Element 1</h3>
-const Elem2 = () => <h3>Element 2</h3>
-export default Documentation
+
+const Elem1 = () => (
+  <Markdown source={atob(element1.split("data:text/x-markdown;base64,")[1])} />
+);
+const Elem2 = () => (
+  <Markdown source={atob(element2.split("data:text/x-markdown;base64,")[1])} />
+);
+
+export default Documentation;
